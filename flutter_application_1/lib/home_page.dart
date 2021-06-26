871caa28bd02bd36e7617228d901ee57348a906c
Yush Chaudhary <Chaudharyayush467@gmail.com>
2021-06-26 22:49:55 +0530

    Update home_page.dart

--- flutter_application_1/lib/home_page.dart
+++ flutter_application_1/lib/home_page.dart
@@ -2,22 +2,19 @@ import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
   final int days = 30;
-  const Homepage({ Key? key }) : super(key: key);
+  const Homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      
-      appBar: AppBar(   
-        backgroundColor: Colors.purple,
-           title: Text("Test App"),
-      ),
-
-      body:Center (
-       child:Container(
-        child: Text("Good $days Day"),
-         )
-    ,)
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Text("Test App"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Good $days Day"),
+          ),
+        ));
   }
 }
