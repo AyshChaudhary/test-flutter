91b1d48b67761801f0d4323b7124ee5bab942aae
Yush Chaudhary <Chaudharyayush467@gmail.com>
2021-06-27 08:29:08 +0530

    object and class

--- flutter_application_1/lib/pages/loginPage.dart
+++ flutter_application_1/lib/pages/loginPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+class LOGINPAGE extends StatelessWidget {
+  const LOGINPAGE({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
